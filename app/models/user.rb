bf4e62c10c0ba186723a090a0fab9e729143a43d
Rostam Mahabadi <RostamMahabadi@gmail.com>
2020-11-28 15:17:57 -0700

    clean up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,10 @@ class User < ApplicationRecord
   validates_confirmation_of :password
   validates_presence_of :password, require: true, :on => :create
   validates_length_of :password, :minimum => 6
+
+  has_many :friends
+  has_many :followed_users, through: :friends
+
+  has_many :followers, foreign_key: :followed_user_id, class_name: "Friends"
+  has_many :follower_users, through: :friends, source: :user
 end
