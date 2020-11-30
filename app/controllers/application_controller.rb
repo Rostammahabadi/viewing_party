71d4fce4a254cdf26335fdb2bd1c4b401efdfb09
Rostam Mahabadi <RostamMahabadi@gmail.com>
2020-11-30 09:37:48 -0700

    added authentication helper method to check if current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :authenticate
   def current_user
     @current_user ||= User.find_by id: session[:user_id] if session[:user_id]
+  end
+
+  def authenticate
+    redirect_to root_path, notice: "You must login or sign up first" if !current_user
   end
 end
